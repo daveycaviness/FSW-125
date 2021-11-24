0d209f2e0303fa8df94daa124fcd3b21a7a3cc70
daveycaviness <85713610+daveycaviness@users.noreply.github.com>
2021-11-23 23:42:11 -0500

    FSW 125 Math

---
+++ Week1/Math/app.js
@@ -0,0 +1,2 @@
+const math = require('./math')
+console.log(math.addition(1, 1), math.subtraction(35, 10), math.multiplication(5, 5), math.division(80, 4))
---
+++ Week1/Math/math.js
@@ -0,0 +1,23 @@
+function addition(firstNum, secondNum){
+    return firstNum + secondNum 
+}
+
+function subtraction(firstNum, secondNum){
+    return firstNum - secondNum 
+}
+
+function multiplication(firstNum, secondNum){
+    return firstNum * secondNum 
+}
+
+function division(firstNum, secondNum){
+    return firstNum / secondNum 
+}
+
+module.exports = {
+    addition,
+    subtraction,
+    multiplication,
+    division
+}
+
